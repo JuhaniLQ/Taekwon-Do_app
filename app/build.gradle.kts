70c46404f2dfc5248b01dc0a4984f17330929f4c
JuhaniLQ <juhani.lq@gmail.com>
2026-01-18 15:18:29 +0200

    Update app version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fi.tkd.itfun"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
